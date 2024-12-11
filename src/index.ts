c12f3945fa326b4a68237a263cf3ecfe52f17aec
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-11 14:30:52 -0300

    Feat: testando rrouter

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`\nServidor rodando na porta ${port}`);
 });
