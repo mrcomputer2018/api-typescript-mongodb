077c7c46f0fda3d64b4dead6abda021717c0e867
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-11 18:43:23 -0300

    Feat: implement user retrieval with MongoDB and add Docker support

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import { GetUsersController } from './controllers/get-users/get-users';
 import dotenv from 'dotenv';
 import express from 'express';
+import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+   const response = await getUsersController.handle();
+
+    res.send(response.body).status(response.statusCode);
 });
 
 app.listen(port, () => {
---
+++ src/repositories/get-users/mongo-get-users.ts
@@ -0,0 +1,15 @@
+import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { User } from "../../models/user";
+
+export class MongoGetUsersRepository implements IGetUsersRepository {
+    async getUsers(): Promise<User[]> {
+        // implementation
+        return [{
+            id: 1,
+            firstName: 'any_first_name',
+            lastName: 'any_last_name',
+            email: 'any_email',
+            password: '123'
+        }]
+    }
+}
