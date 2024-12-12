d26c0a48b861532bb4074b27104984c5be323323
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-12 14:32:13 -0300

    feat: add id to model user

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,8 +1,11 @@
 import { IGetUsersRepository } from '../../controllers/get-users/protocols';
+import { MongoClient } from '../../database/mongo';
 import { User } from '../../models/user';
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
+        const users = await MongoClient.db.collection<User>('users').find().toArray();
+
         // implementation
         return [
             {
