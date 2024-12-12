26d1fcd63133bb6357347d6ae4bd1e02129e3519
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-12 14:43:19 -0300

    feat: criando repository getUsers

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -4,17 +4,10 @@ import { User } from '../../models/user';
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
-        const users = await MongoClient.db.collection<User>('users').find().toArray();
+        //Omit remove uma propriedade de uma interface
+        const users = await MongoClient.db.collection<Omit<User, 'id'>>('users').find().toArray();
 
         // implementation
-        return [
-            {
-                id: 1,
-                firstName: 'any_first_name',
-                lastName: 'any_last_name',
-                email: 'any_email',
-                password: '123',
-            },
-        ];
+        return users.map(({_id, ...rest}) => ({ ...rest, id: _id.toHexString() }));
     }
 }
