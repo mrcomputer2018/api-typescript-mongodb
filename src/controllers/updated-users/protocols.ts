20e246e367c470509f866077b550a3c0580169d8
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-13 08:16:30 -0300

    Feat: create UpdateUserController

--- src/controllers/updated-users/protocols.ts
+++ src/controllers/updated-users/protocols.ts
@@ -1,9 +1,15 @@
+import { HttpRequest } from './../protocols';
 import { User } from "../../models/user";
+import { HttpResponse } from "../protocols";
 
 export interface IUpdateUserParams {
     firstName?: string;
     lastName?: string;
     password?: string;
+}
+
+export interface IUpdateUserController {
+    handle(HttpRequest: HttpRequest<IUpdateUserParams>): Promise<HttpResponse<User>>;
 }
 
 export interface IUpdateUserRepository {
---
+++ src/controllers/updated-users/updated-user.ts
@@ -0,0 +1,54 @@
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+import { IUpdateUserController, IUpdateUserParams, IUpdateUserRepository } from "./protocols";
+
+export class UpdateUserController implements IUpdateUserController {
+    constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
+
+    async handle(HttpRequest: HttpRequest<IUpdateUserParams>): Promise<HttpResponse<User>> {
+       try {
+           const id  = HttpRequest?.params?.id;
+           const body = HttpRequest?.body;
+
+            // verificando se o id foi informado
+            if (!id) {
+                return {
+                        statusCode: 400,
+                        body: 'Id não informado',
+                };
+            }
+
+            const allowedFieldsToUpdate: (keyof IUpdateUserParams)[] = ['firstName', 'lastName', 'password'];
+
+            const someFieldIsNotAllowedToUpdate = body && Object.keys(body).some(key => !allowedFieldsToUpdate.includes(key as keyof IUpdateUserParams));
+
+            if (someFieldIsNotAllowedToUpdate) {
+                return {
+                    statusCode: 400,
+                    body: 'Campo não permitido para atualização',
+                };
+            }
+
+            if (!body) {
+                return {
+                    statusCode: 400,
+                    body: 'Body não informado',
+                };
+            }
+
+            const user = await this.updateUserRepository.updateUser(id, body);
+
+            return {
+                statusCode: 200,
+                body: user,
+            };
+       } catch (error) {
+        console.log(error);
+
+        return {
+            statusCode: 500,
+            body: 'Alguma coisa deu errado. Tente novamente mais tarde.',
+        };
+       }
+    }
+}
