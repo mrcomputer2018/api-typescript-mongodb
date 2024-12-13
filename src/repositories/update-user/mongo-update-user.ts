c2d5fdf74ea87a1803cf2fec18dc63592c48207e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-13 07:50:50 -0300

    feat: create MongoUpdateUserRepository

---
+++ src/repositories/update-user/mongo-update-user.ts
@@ -0,0 +1,24 @@
+import { ObjectId } from 'mongodb';
+import { IUpdateUserParams, IUpdateUserRepository } from '../../controllers/updated-users/protocols'
+import { MongoClient } from '../../database/mongo';
+import { User } from '../../models/user';
+
+export class MongoUpdatedUserRepository implements IUpdateUserRepository {
+    async updateUser(id: string, params: IUpdateUserParams): Promise<User> {
+        await MongoClient.db.collection('users').updateOne(
+            {_id: new ObjectId(id)}, { $set: {...params,} }
+        );
+
+        const user = await MongoClient.db.collection<Omit<User, "id">>('users').findOne({_id: new ObjectId(id)});
+
+        if (!user) {
+            throw new Error('Usuário não encontrado!!!');
+        }
+
+        const { _id, ...rest } = user;
+
+        return { id: _id.toHexString(), ...rest };
+
+    }
+    
+} 
